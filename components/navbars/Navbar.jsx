43ce62ba5eac9c46c972080fb336426ae9cddcc3
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-10 16:37:07 +0100

    Working

--- components/navbars/Navbar.jsx
+++ components/navbars/Navbar.jsx
@@ -1,8 +1,52 @@
+import { MenuItems } from "./MenuItems";
+import { useState } from "react";
+import Link from 'next/link';
+import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "../../public/images/logo.svg";
+
+
+
+
+
 const Navbar = () => {
+    const [active, setActive] = useState(false);
+
+    const handleClick = () => {
+        setActive(!active);
+      };
+
+    const closeLinks = () =>{
+        setActive(false)
+    }
+
     return ( 
         <>
-        <div className="text-3xl font-bold underlinel">This is the navbar trying to test tailwind css let go</div>
-   
+            <nav>
+            <div className="nav-center">
+                <div className="nav-header">
+                <img className="logo" src={logo} alt="logo" loading="lazy"  />
+                <button className="nav-toggle" onClick={handleClick}>
+                    {active ? <FaTimes /> : <FaBars />}
+                </button>
+                </div>
+                <div className="links-container" >
+                <ul className="links">
+                    
+                    {MenuItems.map((link) => {
+                    const { title, id, url, cName } = link;
+                    return (
+                        <li key={id} onClick={closeLinks}>
+                        <Link key={id} className={cName} to={url}>
+                            {title}
+                        </Link>
+                        </li>
+                    );
+                    })}
+                </ul>
+                </div>
+            </div>
+            </nav>
+            
         </>
      );
 }
