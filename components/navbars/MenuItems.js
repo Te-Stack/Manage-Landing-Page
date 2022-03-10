5ffcc441309cffdec701d621ae61f8f1d8b4575c
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-10 15:35:52 +0100

    Working

---
+++ components/navbars/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title:"Pricing",
+        url:"#",
+        cName: "nav-links"
+    },
+    {
+        title:"Product",
+        url:"#",
+        cName: "nav-links"
+    },
+    {
+        title:"About Us",
+        url:"#",
+        cName: "nav-links"
+    },
+    {
+        title:"Careers",
+        url:"#",
+        cName: "nav-links"
+    },
+    {
+        title:"Community",
+        url:"#",
+        cName:"nav-links"
+    }
+]
