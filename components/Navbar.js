af2035813366221e1f3ea75d9029f2e7605d5fe8
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-10 12:47:29 +0100

    Working

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return ( 
+        <>
+        <div className="text-3xl font-bold underlinel">This is the navbar trying to test tailwind css </div>
+   
+        </>
+     );
+}
+ 
+export default Navbar;
