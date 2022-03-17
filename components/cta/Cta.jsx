7ba34b1d33b66d62b65b0280b50e04f70d2050d2
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-17 23:49:32 +0100

    Working

--- components/cta/Cta.jsx
+++ components/cta/Cta.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
             <div className="grid grid-col-12">
                 <div className="col-span-12 xl:col-span-6" >
                     <h1>Bring everyone together to build better products</h1>
-                    <p>Manage makes it simple for software teams tos plan day-to-day tasks while keeping the larger team goals in view </p>
+                    <p>Manage makes it simple for software teams tos plan day-to-day tasks while keeping the larger team goals in views </p>
                     <Button/>
 
 
