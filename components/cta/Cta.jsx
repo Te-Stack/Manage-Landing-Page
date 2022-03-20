c67ef18d99b2e0ad6f09e433b390f5619091c4d0
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-20 13:24:23 +0100

    Working

--- components/cta/Cta.jsx
+++ components/cta/Cta.jsx
@@ -7,15 +7,14 @@ const CTA = () => {
     return ( 
         <div className={styles.cta}>
 
-            <div className="grid grid-col-12">
-                <div className="col-span-12 xl:col-span-6" >
+            <div className={styles.con}>
+                <div className={styles.first}>
                     <h1>Bring everyone together to build better products</h1>
                     <p>Manage makes it simple for software teams tos plan day-to-day tasks while keeping the larger team goals in views  </p>
                     <Button/>
                 </div>
-                <div className="col-span-12 xl:col-span-6" >
+                <div>
                     <img className={styles.img1} src={img} />
-
                 </div>
 
 
