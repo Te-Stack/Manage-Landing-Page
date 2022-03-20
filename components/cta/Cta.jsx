6cdfe5c4429b39ab3837be0bf18abdc0fc9a0dd5
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-20 13:36:00 +0100

    Working

--- components/cta/Cta.jsx
+++ components/cta/Cta.jsx
@@ -9,7 +9,7 @@ const CTA = () => {
 
             <div className={styles.con}>
                 <div className={styles.first}>
-                    <h2 className="text-3xl">Bring everyone together to build better products</h2>
+                    <h2 className="text-4xl font-bold">Bring everyone together to build better products.</h2>
                     <p>Manage makes it simple for software teams tos plan day-to-day tasks while keeping the larger team goals in views  </p>
                     <Button/>
                 </div>
