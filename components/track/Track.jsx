01501a37fb8508562e67300d08bb1c68039fcdcc
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-23 00:47:33 +0100

    Working

--- components/track/Track.jsx
+++ components/track/Track.jsx
@@ -25,7 +25,7 @@ const Track = () => {
                         <button>2</button>
                     </div>
                     <div>
-                        <h6>Advanced built-in reports</h6>
+                        <h6 className="text-lg font-bold">Advanced built-in reports</h6>
                         <p>Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stackholders informed
                         </p>
                     </div>
@@ -35,7 +35,7 @@ const Track = () => {
                         <button>3</button>
                     </div>
                     <div>
-                        <h6>Everything you need in one place</h6>
+                        <h6 className="text-lg font-bold">Everything you need in one place</h6>
                         <p>Stop jumping from one service to another to communicate , store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.
                         </p>
                     </div>
