ffea99d75ff0f8c497c67ade430a577ac5f9dff7
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-23 00:43:45 +0100

    Working

--- components/track/Track.jsx
+++ components/track/Track.jsx
@@ -14,7 +14,7 @@ const Track = () => {
                         <button>1</button>
                     </div>
                     <div>
-                        <h6>Track company-wide progress</h6>
+                        <h6 className="text-lg font-bold">Track company-wide progress</h6>
                         <p>See how your day-to-day tasks fit into the wider vision. 
                             Go from tracking progress at the milestone level all the way done to the smallest of details, Never lose sight of the bigger picture again.
                         </p>
