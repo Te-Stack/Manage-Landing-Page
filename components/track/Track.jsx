ab9279fb205f64b71bb6a89e0f1db1236e9ce10c
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-23 01:41:46 +0100

    Working

--- components/track/Track.jsx
+++ components/track/Track.jsx
@@ -14,7 +14,7 @@ const Track = () => {
                         <button>01</button>
                     </div>
                     <div>
-                        <h6 className="text-lg font-bold">Track company-wide progress</h6>
+                        <h6 className="text-3sm font-bold lg:text-lg">Track company-wide progress</h6>
                         <p>See how your day-to-day tasks fit into the wider vision. 
                             Go from tracking progress at the milestone level all the way done to the smallest of details, Never lose sight of the bigger picture again.
                         </p>
@@ -25,7 +25,7 @@ const Track = () => {
                         <button>02</button>
                     </div>
                     <div>
-                        <h6 className="text-lg font-bold">Advanced built-in reports</h6>
+                        <h6 className="text-3sm font-bold lg:text-lg">Advanced built-in reports</h6>
                         <p>Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stackholders informed
                         </p>
                     </div>
@@ -35,7 +35,7 @@ const Track = () => {
                         <button>03</button>
                     </div>
                     <div>
-                        <h6 className="text-lg font-bold">Everything you need in one place</h6>
+                        <h6 className="text-3sm font-bold lg:text-lg">Everything you need in one place</h6>
                         <p>Stop jumping from one service to another to communicate , store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.
                         </p>
                     </div>
