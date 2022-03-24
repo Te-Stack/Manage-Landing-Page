12fdabea506b2f308862f3f5f7a74c392a78ea82
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-24 11:15:41 +0100

    Working

--- components/carousels/Carousel.jsx
+++ components/carousels/Carousel.jsx
@@ -11,8 +11,8 @@ export default class Carousels extends Component {
         dots: true,
         infinite: false,
         speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         initialSlide: 0,
         responsive: [
           {
@@ -43,7 +43,7 @@ export default class Carousels extends Component {
       };
       return (
         <div>
-          <h2> What they've said </h2>
+          <h1 className="text-lg text-center xl:text-3xl font-bold"> What they've said </h1>
           <Slider {...settings}>
             <div>
               <h3>1</h3>
@@ -63,14 +63,8 @@ export default class Carousels extends Component {
             <div>
               <h3>6</h3>
             </div>
-            <div>
-              <h3>7</h3>
-            </div>
-            <div>
-              <h3>8</h3>
-            </div>
           </Slider>
         </div>
       );
     }
-  }
+}
