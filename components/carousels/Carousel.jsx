7c914b72894fe6d09a01665569831d70ff5bab1e
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-24 10:54:49 +0100

    Working

--- components/carousels/Carousel.jsx
+++ components/carousels/Carousel.jsx
@@ -1,4 +1,8 @@
 import styles from "./carousel.module.scss"
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 
 const Carousel = () => {
     return ( 
