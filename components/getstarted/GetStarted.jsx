c9bd53b89aac3f33b18026ace41d23a8f4e4e324
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-24 16:35:45 +0100

    Working

---
+++ components/getstarted/GetStarted.jsx
@@ -0,0 +1,18 @@
+import Button from "../button/Button";
+
+const GetStarted = () => {
+    return ( 
+        <div>
+            <div>
+                <h1 className="text-2xl xl:text-3xl">
+                    Simplify how your team works today.
+                </h1>
+            </div>
+            <div>
+                <Button/>    
+            </div>
+        </div>
+     );
+}
+ 
+export default GetStarted;
