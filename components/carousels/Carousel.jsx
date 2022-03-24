9d5e5d27b49e72707369e34639709f6ac563383e
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-24 16:16:09 +0100

    Working

--- components/carousels/Carousel.jsx
+++ components/carousels/Carousel.jsx
@@ -27,7 +27,7 @@ export default class Carousels extends Component {
             }
           },
           {
-            breakpoint: 600,
+            breakpoint: 800,
             settings: {
               slidesToShow: 2,
               slidesToScroll: 2,
