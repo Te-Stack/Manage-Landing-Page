5b0656ce0558a075d63588643a58fdc073ae785c
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-24 10:49:22 +0100

    Working

---
+++ components/carousels/Carousel.jsx
@@ -0,0 +1,11 @@
+import styles from "./carousel.module.scss"
+
+const Carousel = () => {
+    return ( 
+        <>
+        <h1>What they've said</h1>
+        </>
+     );
+}
+ 
+export default Carousel;
