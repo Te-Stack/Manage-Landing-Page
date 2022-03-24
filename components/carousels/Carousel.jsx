33975f811fd43d6243cbb90cbab8803fe97a4c15
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-24 12:09:28 +0100

    Working

--- components/carousels/Carousel.jsx
+++ components/carousels/Carousel.jsx
@@ -3,6 +3,11 @@ import styles from "./carousel.module.scss"
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import Image from "next/image"
+// import img1 from "../../public/images/avatar-ali.png"
+// import img2 from "../../public/images/avatar-anisha.png"
+// import img3 from "../../public/images/avatar-richard.png"
+// import img4 from "../../public/images/avatar-shanai.png"
 
 
 export default class Carousels extends Component {
@@ -12,14 +17,14 @@ export default class Carousels extends Component {
         infinite: false,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         initialSlide: 0,
         responsive: [
           {
             breakpoint: 1024,
             settings: {
               slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToScroll: 1,
               infinite: true,
               dots: true
             }
@@ -46,15 +51,27 @@ export default class Carousels extends Component {
           <h1 className="text-lg text-center xl:text-3xl font-bold"> What they've said </h1>
           <Slider {...settings}>
             <div>
-              <h3 className="text-lg xl:text-2xl font-bold">Anisha Li</h3>
+                <img src="/images/avatar-anisha.png" />
+              <h3 className="text-lg xl:text-xl font-bold">Anisha Li</h3>
+              <p>"Manage has Supercharged our team's workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated."</p>
             </div>
 
             <div>
-                <h3 className="text-lg xl:text-2xl font-bold">Ali Bravo</h3>
+                <img src="/images/avatar-ali.png" />
+                <h3 className="text-lg xl:text-xl font-bold">Ali Bravo</h3>
+                <p>"We have been able to cancel so many other subscriptions since using Manage. There is no more cross channel confusion and everyone is much focused."</p>
             </div>
 
             <div>
-              <h3 className="text-lg xl:text-2xl font-bold">Richard Watts</h3>
+                <img src="/images/avatar-richard.png" />
+              <h3 className="text-lg xl:text-xl font-bold">Richard Watts</h3>
+              <p>"Manage allows us to provide structure and provide structure and process. it keeps us organized and focused. i can't stop recommending them to everyone I talk to!"</p>
+            </div>
+
+            <div>
+                <img src="/images/avatar-shanai.png" alt="me" />
+              <h3 className="text-lg xl:text-xl font-bold">Shanai Gough</h3>
+              <p>"Their software allows us to track, manage and collaborate on our projects from anywhere. it keeps the whole team in-sync without being intrusive"</p>
             </div>
           </Slider>
         </div>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const withImages = require("next-images")
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
+}
+
+module.exports = {
+  images: {
+    disableStaticImages: true
+  }
 }
 
 module.exports = (nextConfig,withImages())
